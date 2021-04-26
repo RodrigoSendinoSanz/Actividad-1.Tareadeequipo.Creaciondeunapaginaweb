1577301120915d8aa3f0321d0fe84cdd666ec30c
RodrigoSendinoSanz <78493406+RodrigoSendinoSanz@users.noreply.github.com>
2021-04-26 18:31:00 +0200

    Poner script en su carpeta y linkarlo

---
+++ scripts/colorcambiomenu.js
@@ -0,0 +1,12 @@
+    const maker = document.querySelector('#marker');
+    const item = document.querySelectorAll('ul li a');
+
+    function Indicator(e){
+        marker.style.left = e.offsetLeft+'px';
+        marker.style.width = e.offsetWidth+'px';
+    }
+    item.forEach(link => {
+      link.addEventListener('mousemove', (e) =>{
+          Indicator(e.target);
+      })
+    })
